02008f42a7181f005a30dd2660ac77f75c8e9351
Michael Odere <michael@odereinc.com>
2018-03-19 13:47:22 -0700

    Deleted Json

--- Athena/Stores/RussianStore.swift
+++ Athena/Stores/RussianStore.swift
@@ -1,5 +1,35 @@
 import Foundation
+import SwiftyJSON
 
 struct RussianStore {
-    var topics: [Topic]
+    var topics: [Topic] = []
+
+    init() {
+        populateTopics()
+    }
+
+    mutating func populateTopics() {
+        guard let url = Bundle.main.url(forResource: "Russian", withExtension: "json") else {
+            fatalError("Cannot find Russian.json")
+        }
+
+        do {
+            let data = try Data(contentsOf: url)
+            let jsonData = try JSON(data)
+            
+            guard let topicsData = jsonData["topics"].array else {
+                fatalError("Cannot parse Russian topics data")
+            }
+            
+            
+            for topicJson in topicsData{
+                if let topic = Topic(json: topicJson) {
+                    topics.append(topic)
+                }
+            }
+            
+        } catch {
+            print(error)
+        }
+    }
 }
