4dd832161a82ec1c2ce268cb5c938d8167092969
Michael Odere <michael@odereinc.com>
2018-03-19 10:43:51 -0700

    Seperated variables into words learned and to learn

--- Athena/Models/Topic.swift
+++ Athena/Models/Topic.swift
@@ -1,11 +1,13 @@
 struct Topic {
     var name: String
     var icon: String
-    var words: [Word]
+    var wordsLearned: [Word]
+    var wordsToLearn: [Word]
 
-    init(name: String, icon: String, words: [Word]) {
+    init(name: String, icon: String, wordsLearned: [Word], wordsToLearn: [Word]) {
         self.name = name
         self.icon = icon
-        self.words = words
+        self.wordsLearned = wordsLearned
+        self.wordsToLearn = wordsToLearn
     }
 }
