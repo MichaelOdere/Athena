da1bdb6ff784529a5e07dd1946a5f486b1b0d30f
Michael Odere <michael@odereinc.com>
2018-03-20 16:43:33 -0700

    Add delegation to move between views

--- Athena/Controllers/LearnTopicViewController.swift
+++ Athena/Controllers/LearnTopicViewController.swift
@@ -1,12 +1,33 @@
 import UIKit
 
-class LearTopicViewController: UIViewController {
+protocol DoneHandlerProtocol: class {
+    func nextView(tag: Int)
+}
+
+class LearnTopicViewController: UIViewController {
 
     var introductionToWordView: IntroductionToWordView!
+    var dragToCorrectView: DragToCorrectView!
     var topic: Topic!
 
     override func viewDidLoad() {
         introductionToWordView = IntroductionToWordView(frame: self.view.frame, word: topic.wordsToLearn[0])
+        introductionToWordView.delegate = self
+        dragToCorrectView = DragToCorrectView(frame: self.view.frame)
+        dragToCorrectView.delegate = self
         view.addSubview(introductionToWordView)
+    }
+}
+
+extension LearnTopicViewController: DoneHandlerProtocol {
+    func nextView(tag: Int) {
+        for subview in view.subviews {
+            subview.removeFromSuperview()
+        }
+        if tag == 0 {
+            view.addSubview(dragToCorrectView)
+        } else {
+            view.addSubview(introductionToWordView)
+        }
     }
 }
