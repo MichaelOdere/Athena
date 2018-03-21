cc11f27c2b53bf93951c7bfb568b420bc01c06d2
Michael Odere <michael@odereinc.com>
2018-03-21 00:05:07 -0700

    New colors

--- Athena/Stores/AthenaPallett.swift
+++ Athena/Stores/AthenaPallett.swift
@@ -3,6 +3,8 @@ import UIKit
 public struct AthenaPalette {
     static public let parisGreen = UIColor(red: 0x52, green: 0xDD, blue: 0x6C)
     static public let maximumBlue = UIColor(red: 0x35, green: 0xA1, blue: 0xCC)
+    static public let lightBlue = UIColor(red: 0xAE, green: 0xD4, blue: 0xE6)
+    static public let turquoise = UIColor(red: 0x44, green: 0xE5, blue: 0xE7)
     static public let lightPink = UIColor(red: 0xE0, green: 0xBA, blue: 0xD7)
     static public let rasberryPink = UIColor(red: 0xE3, green: 0x63, blue: 0x97)
     static public let maximumRed = UIColor(red: 0xDE, green: 0x1A, blue: 0x1A)
