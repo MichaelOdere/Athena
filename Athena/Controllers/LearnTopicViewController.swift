52807cefe338a6fe6bb23f341b20dc9c95ed5216
Michael Odere <michael@odereinc.com>
2018-04-01 02:19:32 -0700

    Animation and transition logic

--- Athena/Controllers/LearnTopicViewController.swift
+++ Athena/Controllers/LearnTopicViewController.swift
@@ -1,56 +1,97 @@
 import UIKit
 
+enum PreviusView {
+    case initialView
+    case introductionToWord
+    case dragToCorrectView
+}
+
 enum ResultOfLearn {
     case learned
     case notLearned
-    case answerCorrent
-    case answerWrong
+    case correct
+    case incorrect
 }
 
 protocol DoneHandlerProtocol: class {
-    func nextView(tag: Int)
-    func outcome(result: ResultOfLearn)
+    func nextView(previous: PreviusView, result: ResultOfLearn)
 }
 
 class LearnTopicViewController: UIViewController {
 
     var introductionToWordView: IntroductionToWordView!
     var dragToCorrectView: DragToCorrectView!
+
+    var successView: SuccessView!
+    var failView: FailView!
+
     var topic: Topic!
 
     override func viewDidLoad() {
+        successView = SuccessView(frame: view.frame)
+        failView = FailView(frame: view.frame)
+
         introductionToWordView = IntroductionToWordView(frame: self.view.frame)
         introductionToWordView.delegate = self
+
         dragToCorrectView = DragToCorrectView(frame: self.view.frame)
         dragToCorrectView.delegate = self
 
-        nextView(tag: 0)
+        nextView(previous: .initialView, result: .learned)
     }
 }
 
 extension LearnTopicViewController: DoneHandlerProtocol {
-    func nextView(tag: Int) {
-        for subview in view.subviews {
-            subview.removeFromSuperview()
+
+    func nextView(previous: PreviusView, result: ResultOfLearn) {
+        switch previous {
+        case .initialView:
+            if !showNewWord() {
+                _ = showDragToCorrectView()
+            }
+        case .introductionToWord:
+            if !showDragToCorrectView() {
+                _ = showNewWord()
+            }
+        case .dragToCorrectView:
+            showResult(result: result)
         }
-        if tag == 0 && topic.wordsLearned.count > 0 {
+    }
+
+    func showNewWord() -> Bool {
+        if topic.wordsToLearn.count > 0 {
+            removeAllSubviews()
+            view.addSubview(introductionToWordView)
+            introductionToWordView.sendWord(word: topic.wordsToLearn[0])
+            incrementTopicsProgress()
+            return true
+        }
+        return false
+    }
+
+    func showDragToCorrectView() -> Bool {
+        if topic.wordsLearned.count > 0 {
+            removeAllSubviews()
             view.addSubview(dragToCorrectView)
             dragToCorrectView.dragView.setup()
             let randomIndex = Int(arc4random_uniform(UInt32(topic.wordsLearned.count)))
             let word = topic.wordsLearned[randomIndex]
             dragToCorrectView.dragView.setText(word: word, nativeWords: getRandomWords(word: word))
-        } else if topic.wordsToLearn.count > 0 {
-            view.addSubview(introductionToWordView)
-            introductionToWordView.sendWord(word: topic.wordsToLearn[0])
-            incrementTopicsProgress()
-        } else {
-            nextView(tag: 0)
-//            navigationController?.popViewController(animated: false)
+            return true
         }
+        return false
     }
 
-    func outcome(result: ResultOfLearn) {
-        print(result)
+    func showResult(result: ResultOfLearn) {
+        if result == .correct {
+            view.addSubview(successView)
+            successView.circleView.initAnimations(vc: self)
+        }
+
+        if result == .incorrect {
+            view.addSubview(failView)
+            failView.circleView.initAnimations(vc: self)
+        }
     }
 
     func getRandomWords(word: Word) -> [String] {
@@ -101,5 +142,18 @@ extension LearnTopicViewController: DoneHandlerProtocol {
             topic.wordsLearned.append(topic.wordsToLearn[0])
             topic.wordsToLearn.remove(at: 0)
         }
+    }
+
+    func removeAllSubviews() {
+        for subview in view.subviews {
+            subview.removeFromSuperview()
+        }
+    }
+}
+
+extension LearnTopicViewController: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        removeAllSubviews()
+        nextView(previous: .initialView, result: .correct)
     }
 }
