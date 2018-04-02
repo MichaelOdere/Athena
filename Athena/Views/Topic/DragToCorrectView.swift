afbd56d043dd2cc70f4471dcf14e212568540c09
Michael Odere <michael@odereinc.com>
2018-04-01 21:57:52 -0700

    Removed functionality that was moved to TopicView

--- Athena/Views/Topic/DragToCorrectView.swift
+++ Athena/Views/Topic/DragToCorrectView.swift
@@ -1,12 +1,10 @@
 import UIKit
 
-class DragToCorrectView: UIView {
+class DragToCorrectView: TopicView {
     var dragView: DragFiveView!
-    var progressView: ProgresView!
-    var horizontalPadding: CGFloat = 0
 
     var gl: CAGradientLayer!
-
+    var colors = [AthenaPalette.lightBlue.cgColor, AthenaPalette.turquoise.cgColor]
     weak var delegate: DoneHandlerProtocol? {
         didSet {
             dragView.delegate = delegate
@@ -15,19 +13,9 @@ class DragToCorrectView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        horizontalPadding = frame.width * 0.1
 
-        initGradientColor()
+        initGradientColor(colors: colors)
         initDragFiveView()
-        initProgressView()
-    }
-
-    func initGradientColor() {
-        gl = CAGradientLayer()
-        gl.frame = self.frame
-        gl.colors = [AthenaPalette.lightBlue.cgColor, AthenaPalette.turquoise.cgColor]
-        gl.locations = [0.0, 1.0]
-        self.layer.addSublayer(gl)
     }
 
     func initDragFiveView() {
@@ -36,49 +24,6 @@ class DragToCorrectView: UIView {
         dragView = DragFiveView(frame: mainFrame)
         dragView.backgroundColor = UIColor.clear
         addSubview(dragView)
-    }
-
-    func initProgressView() {
-        progressView = ProgresView()
-        addSubview(progressView)
-
-        progressView.translatesAutoresizingMaskIntoConstraints = false
-
-        let bottom = NSLayoutConstraint(item: progressView,
-                                     attribute: .bottom,
-                                     relatedBy: .equal,
-                                     toItem: self,
-                                     attribute: .bottom,
-                                     multiplier: 1,
-                                     constant: -20)
-        bottom.isActive = true
-
-        let leading = NSLayoutConstraint(item: progressView,
-                                         attribute: .leading,
-                                         relatedBy: .equal,
-                                         toItem: self,
-                                         attribute: .leading,
-                                         multiplier: 1,
-                                         constant: horizontalPadding)
-        leading.isActive = true
-
-        let trailing = NSLayoutConstraint(item: progressView,
-                                          attribute: .trailing,
-                                          relatedBy: .equal,
-                                          toItem: self,
-                                          attribute: .trailing,
-                                          multiplier: 1,
-                                          constant: -horizontalPadding)
-        trailing.isActive = true
-
-        let height = NSLayoutConstraint(item: progressView,
-                                          attribute: .height,
-                                          relatedBy: .equal,
-                                          toItem: nil,
-                                          attribute: .height,
-                                          multiplier: 1,
-                                          constant: frame.height / 10)
-        height.isActive = true
     }
 
     required init?(coder aDecoder: NSCoder) {
