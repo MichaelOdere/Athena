2224c5014ca538a021d1d5d6d6609ef26b52d21c
Michael Odere <michael@odereinc.com>
2018-04-02 17:03:59 -0700

    Moved into seperate file from IntroductionToWordView

--- Athena/Models/ResultOfLearn.swift
+++ Athena/Models/ResultOfLearn.swift
@@ -1,9 +1,36 @@
-//
-//  ResultOfLearn.swift
-//  Athena
-//
-//  Created by Michael Odere on 4/2/18.
-//  Copyright © 2018 michaelodere. All rights reserved.
-//
+enum ResultOfLearn {
+    case none
+    case learned(Word)
+    case incorrect(Word)
+    case correct
+}
 
-import Foundation
+extension ResultOfLearn {
+    func getWord() -> Word? {
+        switch self {
+        case .correct:
+            return nil
+        case .incorrect(let word):
+            return word
+        case .learned(let word):
+            return word
+        case .none:
+            return nil
+        }
+    }
+
+    func isSameType(res: ResultOfLearn) -> Bool {
+        switch self {
+        case .none:
+            if case .none = res { return true}
+        case .correct:
+            if case .correct = res { return true}
+        case .learned:
+            if case .learned = res { return true}
+        case .incorrect:
+            if case .incorrect = res { return true}
+        }
+
+        return false
+    }
+}
