92dfa9fa5e6374b5c9d56cfa9b6db88d0579d849
Michael Odere <michael@odereinc.com>
2018-04-03 20:00:17 -0700

    Added DragLabel

--- Athena/Views/Topic/SupportingViews/DragLabel.swift
+++ Athena/Views/Topic/SupportingViews/DragLabel.swift
@@ -3,13 +3,13 @@ import UIKit
 class DragLabel: UILabel {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         self.backgroundColor = UIColor.clear
         self.textColor = UIColor.white
         self.textAlignment = .center
         self.font = UIFont(name: "HelveticaNeue-Bold", size: 40)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
