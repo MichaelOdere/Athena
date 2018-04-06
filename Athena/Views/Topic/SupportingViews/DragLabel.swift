0b5c0e8623a30ac5a981f7af1f4e84fc73d7d66c
Michael Odere <michael@odereinc.com>
2018-04-06 13:49:15 -0700

    Sizetofit

--- Athena/Views/Topic/SupportingViews/DragLabel.swift
+++ Athena/Views/Topic/SupportingViews/DragLabel.swift
@@ -7,6 +7,7 @@ class DragLabel: UILabel {
         self.backgroundColor = UIColor.clear
         self.textColor = UIColor.white
         self.textAlignment = .center
+        self.adjustsFontSizeToFitWidth = true
         self.font = UIFont(name: "HelveticaNeue-Bold", size: 40)
     }
 
