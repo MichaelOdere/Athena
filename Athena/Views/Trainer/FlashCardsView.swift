369d30874e4dada14ffbc1cfe5918ed5b2020afd
Michael Odere <michael@odereinc.com>
2018-04-23 18:31:43 -0700

    Formatting

--- Athena/Views/Trainer/FlashCardsView.swift
+++ Athena/Views/Trainer/FlashCardsView.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 class FlashCardsView: UIView {
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
