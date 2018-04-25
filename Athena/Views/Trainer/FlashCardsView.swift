6dc56ded89a294a69bcc1f35f943d7efefa6462d
Michael Odere <michael@odereinc.com>
2018-04-24 23:44:32 -0700

    Fetch words

--- Athena/Views/Trainer/FlashCardsView.swift
+++ Athena/Views/Trainer/FlashCardsView.swift
@@ -2,11 +2,59 @@ import UIKit
 
 class FlashCardsView: TrainerView {
 
+    var collectionView: UICollectionView!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        name = "Flash Cards"
+        initCollectionView()
+    }
+
+    func initCollectionView() {
+        collectionView = UICollectionView(frame: CGRect(x: 0,
+                                                        y: frame.height * 0.1,
+                                                        width: frame.width,
+                                                        height: frame.height *  0.9),
+                                          collectionViewLayout: FlashCardCollectionViewLayout())
+
+        collectionView.register(TrainerCollectionCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.backgroundColor = UIColor.clear
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        addSubview(collectionView)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension FlashCardsView: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return words.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let word = words[indexPath.row]
+
+        var cell: TrainerCollectionCell!
+
+        if let dqCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+            as? TrainerCollectionCell {
+            cell = dqCell
+        } else {
+            cell = TrainerCollectionCell()
+        }
+
+        cell.nameLabel.text = word.english
+        return cell
     }
 }
--- Athena/Views/Trainer/TrainerView.swift
+++ Athena/Views/Trainer/TrainerView.swift
@@ -2,4 +2,5 @@ import UIKit
 
 class TrainerView: UIView {
     var name: String = ""
+    var words: [Word] = []
 }
